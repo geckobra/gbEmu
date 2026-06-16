7e6b18141c5fbda95f8c8904043d9ef817f28fed
Ferni <juanlopezfernandes@gmail.com>
2026-06-16 22:58:44 +0200

    Initialization of WRAM and CPU

--- src/main.c
+++ src/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include "CPU.h"
+#include "MMU.h"
 
 int main(){
     printf("Hello, world!\n");
+
+    cpu_init();
+    write_memory(0xC000, 0x06);
+    write_memory(0xC001, 200);
+
+    while (1){
+        run_cpu();
+    }
 
     return 0;
 }
