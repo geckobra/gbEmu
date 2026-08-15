0f9dd3470eb931b3f11dada3336d364dc0d0847c
Ferni <juanlopezfernandes@gmail.com>
2026-08-15 19:30:58 +0200

    Added memory initialization and rom load from argv

--- src/main.c
+++ src/main.c
@@ -2,13 +2,22 @@
 #include "CPU.h"
 #include "MMU.h"
 
-int main(){
+int main(int argc, char* argv[]){
     printf("Hello, world!\n");
 
-    cpu_init();
-    write_memory(0xC000, 0x06);
-    write_memory(0xC001, 200);
+    if (argc < 2){
+        printf("No ROM provided, terminating\n");
+        return 0;
+    }
 
+    mem_init();
+    cpu_init();
+
+    printf("Loading ROM at %s\n", argv[1]);
+    if (load_ROM(argv[1]) < 0){
+        printf("Couldn't read ROM!!\n");
+    }
+    
     while (1){
         run_cpu();
     }
