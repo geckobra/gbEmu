ccfb2781223f4c29f9fec84314535b6eaf0a9f07
Ferni <juanlopezfernandes@gmail.com>
2026-08-15 19:30:32 +0200

    Added proper initialization, debug and fixed several bugs in ALU operations

--- src/CPU.c
+++ src/CPU.c
@@ -2,6 +2,41 @@
 #include "MMU.h"
 #include <string.h>
 
+#define TRACE_SIZE 256
+
+typedef struct {
+    uint16_t pc;
+    uint8_t opcode;
+    uint8_t a, f, b, c, d, e, h, l;
+    uint16_t sp;
+} CpuTrace;
+
+CpuTrace trace_buffer[TRACE_SIZE];
+size_t trace_index = 0;
+
+void record_trace(uint8_t opcode, uint16_t current_pc) {
+    trace_buffer[trace_index] = (CpuTrace){
+        .pc = current_pc,
+        .opcode = opcode,
+        .a = cpu_registers.a, .f = cpu_registers.f,
+        .b = cpu_registers.b, .c = cpu_registers.c,
+        .d = cpu_registers.d, .e = cpu_registers.e,
+        .h = cpu_registers.h, .l = cpu_registers.l,
+        .sp = cpu_registers.sp
+    };
+    trace_index = (trace_index + 1) % TRACE_SIZE;
+}
+
+void dump_trace(void) {
+    printf("\n=== LAST %d EXECUTED INSTRUCTIONS ===\n", TRACE_SIZE);
+    for (size_t i = 0; i < TRACE_SIZE; i++) {
+        size_t idx = (trace_index + i) % TRACE_SIZE;
+        CpuTrace* t = &trace_buffer[idx];
+        printf("PC: 0x%04X | OP: 0x%02X | A: %02X F: %02X | BC: %02X%02X | DE: %02X%02X | HL: %02X%02X | SP: %04X\n",
+               t->pc, t->opcode, t->a, t->f, t->b, t->c, t->d, t->e, t->h, t->l, t->sp);
+    }
+}
+
 sm83_registers_t cpu_registers = {};
 bool isHalted = false;
 bool interrupts_enabled = false;
@@ -10,10 +45,28 @@ uint8_t ei_delay = 2;
 uint8_t next_instruction = 0x0; //next instruction to be executed
 uint64_t total_t_cycles = 0;
 
-void cpu_init(){
-    memset(&cpu_registers, 0, sizeof(cpu_registers));
+FILE* cpu_log_file = NULL;
 
-    cpu_registers.pc = 0xC000;
+void init_logging(void) {
+    cpu_log_file = fopen("cpu_trace.log", "w");
+
+    if (cpu_log_file == NULL){
+        printf("No log file\n");
+    }
+}
+
+void cpu_init(){
+    //initialize registers to default values
+    cpu_registers.a = 0x01;
+    cpu_registers.f = 1 << 7;
+    cpu_registers.b = 0x00;
+    cpu_registers.c = 0x13;
+    cpu_registers.d = 0x00;
+    cpu_registers.e = 0xD8;
+    cpu_registers.h = 0x01;
+    cpu_registers.l = 0x4D;
+    cpu_registers.sp = 0xFFFE;
+    cpu_registers.pc = 0x0100;
 }
 
 //fetches the next instruction from WRAM memory
@@ -32,10 +85,17 @@ void run_cpu(){
     }
 
     //if the cpu is halted, don't fetch next instruction
-    if (isHalted){
+    if (isHalted) {
         t_cycles_executed = 4;
-    } else{
+    } else {
+        uint16_t current_pc = cpu_registers.pc;
         next_instruction = fetch();
+
+        if (cpu_log_file) {
+            fprintf(cpu_log_file, "PC: 0x%04X | OP: 0x%02X | A: 0x%02X | F: 0x%02X | SP: 0x%04X\n",
+                    current_pc, next_instruction, cpu_registers.a, cpu_registers.f, cpu_registers.sp);
+        }
+
         t_cycles_executed = execute(next_instruction);
     }
     
@@ -81,16 +141,13 @@ uint8_t alu_adc8(uint8_t val){
 
 uint8_t alu_sub8(uint8_t val){
     //performs substraction to the accumulation register and sets corresopnding flags
-
-    uint8_t flags_mask = 0x0;
     uint8_t a_val = cpu_registers.a;
-
     uint8_t result = a_val - val;
-
-    flags_mask |= 1 << 6; //set N flag
-    if (result == 0) flags_mask |= 1 << 7; //set Z flag
-    if (((a_val & 0x0F) - (val & 0x0F)) & 0x10) flags_mask |= 1 << 5; //set H-carry flag
-    if (val > a_val) flags_mask |= 1 << 4; //if value is higher than regA set Carry flag
+    
+    uint8_t flags_mask = (1<<6);
+    if (result == 0) flags_mask |= (1 << 7); //set Z flag
+    if (((a_val & 0x0F) < (val & 0x0F))) flags_mask |= (1 << 5); //set H-carry flag
+    if (val > a_val) flags_mask |= (1 << 4); //if value is higher than regA set Carry flag
 
     cpu_registers.f = flags_mask;
 
@@ -99,17 +156,16 @@ uint8_t alu_sub8(uint8_t val){
 
 uint8_t alu_subc8(uint8_t val){
 
-    uint8_t flags_mask = 0x0;
+    uint8_t flags_mask = 1 << 6;
 
     uint8_t a_val = cpu_registers.a;
     uint8_t carry_flag = (cpu_registers.f & (1 << 4)) ? 1 : 0;
 
     uint8_t result = a_val - val - carry_flag;
 
-    flags_mask |= 1 << 6; //set N flag
     if (result == 0) flags_mask |= 1 << 7;
-    if (((a_val & 0x0F) - (val & 0x0F) - carry_flag) & 0x10) flags_mask |= 1 << 5; //set H-carry flag
-    if ((int)a_val - (int)val - (int)carry_flag < 0) flags_mask |= 1 << 4; //borrow if the total value to be substracted is greater than regA
+    if ((a_val & 0x0F) < ((val & 0x0F) + carry_flag)) flags_mask |= 1 << 5; //set H-carry flag
+    if (a_val < (val + carry_flag)) flags_mask |= 1 << 4; //borrow if the total value to be substracted is greater than regA
 
     cpu_registers.f = flags_mask;
 
@@ -117,35 +173,25 @@ uint8_t alu_subc8(uint8_t val){
 }
 
 uint8_t inc_r8(uint8_t reg){
-
-    uint8_t flags_mask = 0x0;
     uint8_t result = reg+1;
 
-    if (result == 0) flags_mask |= 1 << 7;
-    if ((result & 0x0F) == 0x0F) flags_mask |= 1 << 5;
-
-    flags_mask |= (cpu_registers.f & (1 << 4)); //carry flag shouldn't be affected by the operation
-
-    cpu_registers.f = flags_mask;
+    cpu_registers.f &= ~(1 << 7 | 1 << 6 | 1 << 5); // Clear Z, N, H (Keep C!)
+    if (result == 0) cpu_registers.f |= (1 << 7);      // Set Z
+    if ((reg & 0x0F) == 0x0F) cpu_registers.f |= (1 << 5);
     
     return result;
 }
 
-uint8_t dec_r8(uint8_t reg){
-    //decrement reg by one and set flags accordingly
-
-    uint8_t flags_mask = 0x0;
-    uint8_t result = reg - 1;
-
-    flags_mask |= (cpu_registers.f & 0x10); //keep contents of carry flag
-    flags_mask |= (1<<6); //set N flag
-
-    if (result == 0) flags_mask |= (1<<7);
-    if ((reg & 0x0F) == 0) flags_mask |= (1<<5); //set H flag if there is borrow from bit 3 to bit 4
-
-    cpu_registers.f = flags_mask;
-
-    return result;
+uint8_t dec_r8(uint8_t val) {
+    uint8_t res = val - 1;
+    
+    cpu_registers.f &= ~(1 << 7 | 1 << 5); // Clear Z, H (Keep C!)
+    cpu_registers.f |= (1 << 6);           // Set N = 1
+    
+    if (res == 0) cpu_registers.f |= (1 << 7);      // Set Z
+    if ((val & 0x0F) == 0x00) cpu_registers.f |= (1 << 5); // Set H (borrow from bit 4)
+    
+    return res;
 }
 
 uint16_t alu_add16(uint16_t reg1, uint16_t reg2){
@@ -211,20 +257,12 @@ uint8_t xor_r8(uint8_t reg){
 }
 
 uint8_t not(){
-
-    uint8_t flags_mask = 0x0;
     uint8_t a_val = cpu_registers.a;
-
     uint8_t result = ~a_val; //bitwise NOT
 
-    flags_mask |= cpu_registers.f; //keep the value of Z and C flags
-
-    //set N and H flags to 1
-    flags_mask |= 1 << 5;
-    flags_mask |= 1 << 6;
+    uint8_t flags_mask = (cpu_registers.f & 0x90) | (1 << 6) | (1 << 5);
 
     cpu_registers.f = flags_mask;
-
     return result;
 }
 
@@ -263,14 +301,16 @@ uint8_t rl_r8(uint8_t reg){
 
 uint8_t rrc_r8(uint8_t reg){
     //perform circular right rotation of reg
+    uint8_t flags_mask = 0x0;
 
     //standard right circular byte rotation
     uint8_t lsb = reg & 1;
     uint8_t result = (reg>>1) | (lsb<<7);
 
-    if (result == 0) cpu_registers.f |= 1 << 7;
-    cpu_registers.f |= (lsb << 4); //set carry flag to value of LSB
+    if (result == 0) flags_mask |= 1 << 7;
+    flags_mask |= (lsb << 4); //set carry flag to value of LSB
 
+    cpu_registers.f = flags_mask;
     return result;
 }
 
