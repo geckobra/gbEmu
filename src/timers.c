1fcdcfe721f10d382d3d483c483dbfd8b865c32b
Ferni <juanlopezfernandes@gmail.com>
2026-08-19 00:34:37 +0200

    Created helper to update clock configuration from MMU.c without reading and writing directly from the module

--- src/timers.c
+++ src/timers.c
@@ -14,11 +14,30 @@ typedef struct{
 typedef struct{
     uint8_t clk_enabled;
     uint8_t clk_freq;
-    uint8_t clk_IR_value;
+    uint8_t clk_IR_value; //value to set TIMA after an overflow
 } TIMA_cfg_t;
 
 TIMA_cfg_t timer_cfg;
 timer_data_t timers;
+
+uint8_t get_DIV() {
+    return timers.DIV;
+}
+
+uint8_t get_TIMA() {
+    return timers.TIMA;
+}
+
+void reset_DIV() {
+    timers.DIV = 0;
+    timers.div_cycles = 0;
+}
+
+void update_timer_settings(uint8_t enable, uint8_t freq, uint8_t IR_value){
+    timer_cfg.clk_enabled = enable;
+    timer_cfg.clk_freq = freq;
+    timer_cfg.clk_IR_value = IR_value;
+}
 
 void timers_init(){
     timer_cfg.clk_enabled = 0;
@@ -34,30 +53,30 @@ void timers_init(){
 
 void tick_timers(uint8_t t_cycles){
     //read TIMA clock settings before ticking it
-    uint8_t TAC_val = read_memory(0xFF07);
+    uint8_t m_cycles = t_cycles / 4;
+    if (m_cycles == 0) m_cycles = 1;
 
-    timers.div_cycles += (t_cycles / 4);
-
-    //update TIMA settings
-    timer_cfg.clk_enabled = (TAC_val >> 2) & 1;
-    timer_cfg.clk_freq = (TAC_val) & 0x03;
-
-    if (timers.div_cycles > 64){
+    timers.div_cycles += m_cycles;
+    while (timers.div_cycles >= 64) {
         timers.DIV++;
-        timers.div_cycles = 0;
+        timers.div_cycles -= 64;
     }
 
     if (timer_cfg.clk_enabled){
-        timers.tima_cycles += (t_cycles / 4);
+        timers.tima_cycles += m_cycles;
+        
+        uint16_t freq_limit = TIMA_FREQS[timer_cfg.clk_freq];
+        while (timers.tima_cycles >= freq_limit) {
+            timers.tima_cycles -= freq_limit;
+            
+            last_TIMA_val = timers.TIMA;
+            timers.TIMA++;
 
-        if (timers.tima_cycles > TIMA_FREQS[timer_cfg.clk_freq]){
-            last_TIMA_val = timers.TIMA++;
-            timers.tima_cycles = 0;
-        }
-
-        if (timers.TIMA == 0x00 && last_TIMA_val == 0xFF){
-            timers.TIMA = read_memory(0xFF06); //set value to that of the Timer Modulo
-            set_timer_IR();
+            if (timers.TIMA == 0x00 && last_TIMA_val == 0xFF){
+                timers.TIMA = timer_cfg.clk_IR_value; // Timer Modulo
+                uint8_t IF = mmu_get_if();
+                mmu_set_if(IF | (1 << 2));
+            }
         }
     }
 }
--- src/timers.h
+++ src/timers.h
@@ -2,3 +2,7 @@
 
 void timers_init();
 void tick_timers(uint8_t);
+void update_timer_settings(uint8_t, uint8_t, uint8_t);
+uint8_t get_DIV();
+uint8_t get_TIMA();
+void reset_DIV();
